b6df11c423d73fe686918d9e0c163ab1d5a1397e
devheyrin <beforerin@gmail.com>
2022-10-25 20:29:31 +0900

    Update detail

--- src/main/java/com/notax/category/CategoryController.java
+++ src/main/java/com/notax/category/CategoryController.java
@@ -1,4 +1,12 @@
 package com.notax.category;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CategoryController {
+    @RequestMapping("/main-category")
+    public String mainCategory() {
+        return "main-category";
+    }
 }
