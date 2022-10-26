d4c4410ace28756be585c617b9805efa4dfd7759
park <skypark5205@naver.com>
2022-10-26 14:18:09 +0900

    make categoryDAO, VO, service, product

---
+++ src/main/java/com/notax/detail/DetailService.java
@@ -0,0 +1,17 @@
+package com.notax.detail;
+
+
+import com.notax.vo.ProductVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DetailService {
+
+    @Autowired
+    DetailDAO detailDAO;
+
+    public ProductVO selectProductById() {
+        return detailDAO.selectProductById();
+    }
+}
