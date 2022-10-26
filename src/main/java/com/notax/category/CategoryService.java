0b8dec61ff43b2f2d2b74be066d5c4017db9924b
park <skypark5205@naver.com>
2022-10-26 20:58:48 +0900

    select main, category

--- src/main/java/com/notax/category/CategoryService.java
+++ src/main/java/com/notax/category/CategoryService.java
@@ -5,13 +5,27 @@ import com.notax.vo.ProductCategoryVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class CategoryService {
 
     @Autowired
     CategoryDAO categoryDAO;
 
-    public ProductCategoryVO selectProductByMain() {
-        return categoryDAO.selectProductByMain();
+    public ArrayList<ProductCategoryVO> selectProductByMain(int main_no) {
+        return categoryDAO.selectProductByMain(main_no);
+    }
+    public ArrayList<ProductCategoryVO> selectDuty(){
+        return categoryDAO.selectDuty();
+    }
+    public ArrayList<ProductCategoryVO> selectMiddle(int main_no){
+        return categoryDAO.selectMiddle(main_no);
+    }
+    public ArrayList<ProductCategoryVO> selectBrand(int main_no){
+        return categoryDAO.selectBrand(main_no);
+    }
+    public ArrayList<ProductCategoryVO> selectMain(){
+        return categoryDAO.selectMain();
     }
 }
--- src/main/java/com/notax/vo/ProductCategoryVO.java
+++ src/main/java/com/notax/vo/ProductCategoryVO.java
@@ -4,9 +4,14 @@ import lombok.Data;
 
 @Data
 public class ProductCategoryVO {
+    private String dfs_name;
+    private int rownum;
+    private int main_no;
     private String brand_name;
     private String pd_name;
     private String pd_date;
     private String pdphoto_link;
     private int pd_by_dfs_price;
+    private String mid_name;
+    private String main_name;
 }
