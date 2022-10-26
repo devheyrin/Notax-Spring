f4135935c5f866476af4298bca1d41a944c4f0d7
panggu9026 <97830290+panggu9026@users.noreply.github.com>
2022-10-26 20:20:30 +0900

    Add mainMVC
    메인MVC 추가

--- src/main/java/com/notax/main/MainController.java
+++ src/main/java/com/notax/main/MainController.java
@@ -1,4 +1,21 @@
 package com.notax.main;
 
+
+import com.notax.vo.MainRankingVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+    @Autowired
+    MainService mainService;
+
+    @RequestMapping("/mainRank")
+    public String mainRank() {
+        MainRankingVO mainRankingVO = mainService.selectRank();
+        System.out.println("MainRankingVO = " + mainRankingVO);
+        return "mainRank";
+    }
+
 }
---
+++ src/main/java/com/notax/vo/MainRankingVO.java
@@ -0,0 +1,13 @@
+package com.notax.vo;
+
+
+import lombok.Data;
+
+@Data
+public class MainRankingVO {
+    private String pd_name;
+    private int PD_NO;
+    private int PD_BY_DFS_SALE;
+    private int PD_PRICE;
+    private int PD_SALE_PRICE;
+}
