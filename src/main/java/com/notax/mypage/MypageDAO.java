bf75b535df3c518f5472a0063366ef7732ea074c
jeongjuz <115452909+jeongjuz@users.noreply.github.com>
2022-10-26 17:12:45 +0900

    update mypage

---
+++ src/main/java/com/notax/mypage/MypageDAO.java
@@ -0,0 +1,9 @@
+package com.notax.mypage;
+
+import com.notax.vo.MemberMdfVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MypageDAO {
+    public MemberMdfVO updateMemberMdfById();
+}
