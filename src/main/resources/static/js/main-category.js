d86c803c615156891284b90856b29c28adb0f3ca
park <skypark5205@naver.com>
2022-10-28 22:46:04 +0900

    update main, middle

--- src/main/resources/static/js/main-category.js
+++ src/main/resources/static/js/main-category.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('.slider').bxSlider({
         auto: true
     });
+
+    /* 이동 */W
+    $('.feastSelect_example-2 li').click(function () {
+        console.log($(this).attr('id'));
+        location.href = `/main-category/id=${$(this).attr('id')}`;
+    });
 });
