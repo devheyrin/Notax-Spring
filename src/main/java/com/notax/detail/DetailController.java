3cf576f28ed092e161510e8763caeee665703cfe
devheyrin <beforerin@gmail.com>
2022-10-30 16:05:55 +0900

    Add min-price
    - 최저가 추가

--- src/main/java/com/notax/detail/DetailController.java
+++ src/main/java/com/notax/detail/DetailController.java
@@ -3,6 +3,7 @@ package com.notax.detail;
 import com.notax.category.CategoryDAO;
 import com.notax.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,7 +11,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DetailController {
@@ -35,22 +38,30 @@ public class DetailController {
         ArrayList<ProductCategoryVO> middleList = categoryService.selectMiddle(main_no);
 
         String user_id = (String) session.getAttribute("SS_userId");
-        System.out.println("user_id = " + user_id);
-        boolean liked = detailService.isLiked(pd_no, user_id);
-        model.addAttribute("liked", liked);
 
+
+        boolean liked = false;
+        if (user_id != null) liked = detailService.isLiked(pd_no, user_id);
+
+        Map<String, Integer> minPriceMap = minPrice(productByDFSVOList);
+   
+        int min_price = minPriceMap.get("min_price");
+        int sale_percent = minPriceMap.get("sale_percent");
         String mainname = categoryService.mainname(main_no);
+        String mid_name = categoryService.mid_name(main_no, mid_cate_no);
+
+        model.addAttribute("min_price", min_price);
+        model.addAttribute("sale_percent", sale_percent);
+        model.addAttribute("liked", liked);
         model.addAttribute("mainname", mainname);
         model.addAttribute("main_no", main_no);
-
-        String mid_name = categoryService.mid_name(main_no, mid_cate_no);
         model.addAttribute("mid_name", mid_name);
-
         model.addAttribute("product", productVO);
         model.addAttribute("priceList", productByDFSVOList);
         model.addAttribute("productPhotoList", productPhotoVOList);
         model.addAttribute("mainList", mainList);
         model.addAttribute("middleList", middleList);
+
         return "detail";
     }
 
@@ -58,7 +69,6 @@ public class DetailController {
     @RequestMapping("/detail/{pd_no}/like")
     public String like(@PathVariable("pd_no") int pd_no,
                        @RequestParam String userid) {
-        System.out.println("userid = " + userid);
         detailService.saveInterestedItem(pd_no, userid);
         return "ok";
     }
@@ -67,8 +77,23 @@ public class DetailController {
     @RequestMapping("/detail/{pd_no}/dislike")
     public String dislike(@PathVariable("pd_no") int pd_no,
                        @RequestParam String userid) {
-        System.out.println("userid = " + userid);
         detailService.deleteInterestedItem(pd_no, userid);
         return "ok";
+    }
+    
+    private Map<String, Integer> minPrice(List<ProductByDFSVO> productByDFSVOList) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        int price = 1000000000;
+        int sale_percent = 100;
+        for (ProductByDFSVO productByDFSVO : productByDFSVOList) {
+            int total_price = productByDFSVO.getTotal_price();
+            if (total_price < price) {
+                price = total_price;
+                sale_percent = productByDFSVO.getSale_percent();
+            }
+        }
+        resultMap.put("min_price", price);
+        resultMap.put("sale_percent", sale_percent);
+        return resultMap;
     }
 }
