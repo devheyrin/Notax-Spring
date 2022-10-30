3c68060349babc48f44b5bc5c8b9299aaa7a64d5
jeongjuz <115452909+jeongjuz@users.noreply.github.com>
2022-10-30 16:17:22 +0900

    Commit memberupdate

---
+++ src/main/java/com/notax/membership/MembershipDAO.java
@@ -0,0 +1,12 @@
+package com.notax.membership;
+import com.notax.vo.MembershipMdfVO;
+import org.apache.ibatis.annotations.Mapper;
+
+/*
+
+@Mapper
+public interface MembershipDAO {
+
+    public void updateMembership
+}
+*/
